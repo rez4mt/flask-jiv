48f8a5706bedeeb99aa0ede756433ef7ceb1e18b
Reza Motlagh <reza.motlagh@floward.com>
2021-05-11 11:20:15 +0300

    Initial version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="Flask JIV",
-    version="0.0.3",
+    version="0.0.1",
     description="Validates JSON request body for flask endpoints",
     long_description=README,
     long_description_content_type="text/markdown",
